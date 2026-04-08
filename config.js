ebcbdd758fd59825a5fc348b3e3a17b3851c4b41
nhsoft.lsd <nhsoft.lsd@hotmail.com>
2026-04-08 22:05:18 +0800

    init

---
+++ config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = {
+    appStoreUrl: 'https://apps.apple.com/us/app/work-hours-tracker/id6760966627',
+    privacyPolicyUrl: 'https://docs.google.com/document/d/e/2PACX-1vRZLpSRlTIAAfOwO1IzSW_6W9f2hinRDwuHcwswJlRFM3PgCmpSxfqg0oqBcJYd9FQ90Zm7E81_pYHT/pub',
+    contactEmail: 'worktracker@example.com',
+    appName: 'Work Hours Tracker',
+    appNameZh: '工时追踪器',
+    appDescription: 'Track work hours, calculate overtime, manage jobs - all in one app.'
+};
